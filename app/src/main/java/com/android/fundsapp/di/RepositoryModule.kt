a3643821e7f74fa2de2c83328cfe7cb84597e2d4
Azevedo,William <will.pasco.az@gmail.com>
2019-10-22 22:26:27 -0300

    Providing the repository by di.

--- app/src/main/java/com/android/fundsapp/di/RepositoryModule.kt
+++ app/src/main/java/com/android/fundsapp/di/RepositoryModule.kt
@@ -1,7 +1,14 @@
 package com.android.fundsapp.di
 
+import com.android.fundsapp.fundslist.FundsRepository
+import com.android.fundsapp.service.FundsService
 import dagger.Module
+import dagger.Provides
 
 @Module
 class RepositoryModule {
+
+
+    @Provides
+    fun providesFundsRepository(service: FundsService) = FundsRepository(service)
 }
