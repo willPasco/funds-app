fc1542ad7722c04670f2e901d5fe4909972ba01f
Azevedo,William <will.pasco.az@gmail.com>
2019-10-22 22:26:35 -0300

    Testing api request.

--- app/src/main/java/com/android/fundsapp/MainActivity.kt
+++ app/src/main/java/com/android/fundsapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.android.fundsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.fundsapp.fundslist.FundsRepository
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var repository: FundsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppApplication.getMainComponent().inject(this)
+
+        repository.getFunds()
     }
 }
