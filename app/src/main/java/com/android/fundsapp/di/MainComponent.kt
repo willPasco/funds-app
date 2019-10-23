851427fce3bd2efcba81308423407fc87ea9aa2a
Azevedo,William <will.pasco.az@gmail.com>
2019-10-22 21:33:47 -0300

    Creating another structures modules.

--- app/src/main/java/com/android/fundsapp/di/MainComponent.kt
+++ app/src/main/java/com/android/fundsapp/di/MainComponent.kt
@@ -3,7 +3,12 @@ package com.android.fundsapp.di
 import com.android.fundsapp.MainActivity
 import dagger.Component
 
-@Component(modules = [NetworkModule::class])
+@Component(
+    modules = [NetworkModule::class,
+        PresenterModule::class,
+        RepositoryModule::class,
+        ModelModule::class]
+)
 interface MainComponent {
 
     fun inject(mainActivity: MainActivity)
---
+++ app/src/main/java/com/android/fundsapp/di/PresenterModule.kt
@@ -0,0 +1,4 @@
+package com.android.fundsapp.di
+
+class PresenterModule {
+}
