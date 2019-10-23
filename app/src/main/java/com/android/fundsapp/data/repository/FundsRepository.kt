7ac4d7c5c5bb37fd1349987d2fea1edd651c1d44
Azevedo,William <will.pasco.az@gmail.com>
2019-10-23 00:42:42 -0300

    Moving the code in wrong file.

--- app/src/main/java/com/android/fundsapp/data/repository/FundsRepository.kt
+++ app/src/main/java/com/android/fundsapp/data/repository/FundsRepository.kt
@@ -1,25 +1,18 @@
-package com.android.fundsapp.fundslist
+package com.android.fundsapp.data.repository
 
 import android.annotation.SuppressLint
-import android.util.Log
+import com.android.fundsapp.data.entity.FundResponse
 import com.android.fundsapp.data.service.FundsService
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class FundsRepository @Inject constructor(private val service: FundsService) {
 
     @SuppressLint("CheckResult")
-    fun getFunds() {
-        service.getFunds("100", "0", "fund_detail_full")
+    fun getFunds(): Single<List<FundResponse>> {
+        return service.getFunds("100", "0", "fund_detail_full")
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
-            .subscribe(
-                { response ->
-                    response.forEach {
-                        Log.i("test", "${it.simpleName} ${it.operability.minimumApplication}")
-                    }
-                },
-                { error -> Log.e("test", error.toString()) }
-            )
     }
 }
