b2213c0acfeb9830f3c9b7353b0549a8cc6181a3
Azevedo,William <will.pasco.az@gmail.com>
2019-10-23 22:15:56 -0300

    Renaming the main activity.

--- app/src/main/java/com/android/fundsapp/di/MainComponent.kt
+++ app/src/main/java/com/android/fundsapp/di/MainComponent.kt
@@ -1,6 +1,6 @@
 package com.android.fundsapp.di
 
-import com.android.fundsapp.MainActivity
+import com.android.fundsapp.fundslist.FundsListActivity
 import com.android.fundsapp.core.BaseActivity
 import com.android.fundsapp.core.BasePresenter
 import com.android.fundsapp.domain.presenter.FundsListPresenter
@@ -17,7 +17,7 @@ import javax.inject.Singleton
 interface MainComponent {
 
     fun inject(baseActivity: BaseActivity)
-    fun inject(mainActivity: MainActivity)
+    fun inject(mainActivity: FundsListActivity)
 
     fun inject(basePresenter: BasePresenter)
     fun inject(fundsListPresenter: FundsListPresenter)
--- app/src/main/java/com/android/fundsapp/fundslist/FundsListActivity.kt
+++ app/src/main/java/com/android/fundsapp/fundslist/FundsListActivity.kt
@@ -1,19 +1,21 @@
-package com.android.fundsapp
+package com.android.fundsapp.fundslist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.android.fundsapp.AppApplication
+import com.android.fundsapp.R
 import com.android.fundsapp.domain.FundsListView
 import com.android.fundsapp.domain.presenter.FundsListPresenter
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), FundsListView {
+class FundsListActivity : AppCompatActivity(), FundsListView {
 
     @Inject
     lateinit var presenter: FundsListPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_funds_list)
 
         AppApplication.getMainComponent().inject(this)
 
